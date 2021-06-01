56fb58697413e3636135c9ba4874f3ea392f5035
csarraud <clementsarraud@gmail.com>
2021-06-01 20:14:15 +0200

    create project

---
+++ include/pathfinding.hpp
@@ -0,0 +1,15 @@
+#ifndef PATHFINDING_H
+#define PATHFINDING_H
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Pathfinding
+{
+	private:
+
+	public:
+		Pathfinding();
+		~Pathfinding();
+};
+#endif
---
+++ src/pathfinding.cpp
@@ -0,0 +1,16 @@
+#include "pathfinding.hpp"
+
+Pathfinding::Pathfinding()
+{
+
+}
+
+Pathfinding::~Pathfinding()
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    
+}
