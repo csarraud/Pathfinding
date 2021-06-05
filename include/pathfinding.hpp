0fbb1d954f454a42983cf63166bd4db74f2f9a3f
csarraud <clementsarraud@gmail.com>
2021-06-05 18:48:41 +0200

    create window + update gitignore

--- include/pathfinding.hpp
+++ include/pathfinding.hpp
@@ -4,12 +4,23 @@
 
 #include <SFML/Graphics.hpp>
 
+#define X_NODES 20
+#define Y_NODES 20
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 500
+#define NODE_WIDTH (WINDOW_WIDTH / X_NODES)
+#define NODE_HEIGHT (WINDOW_HEIGHT / Y_NODES)
+
 class Pathfinding
 {
 	private:
+		sf::RenderWindow window;
 
 	public:
 		Pathfinding();
+		Pathfinding(int width, int height, int x_nodes, int y_nodes);
 		~Pathfinding();
+
+		void loop();
 };
 #endif
--- src/pathfinding.cpp
+++ src/pathfinding.cpp
@@ -1,8 +1,14 @@
 #include "pathfinding.hpp"
 
 Pathfinding::Pathfinding()
+    : Pathfinding(WINDOW_WIDTH, WINDOW_HEIGHT, X_NODES, Y_NODES)
 {
+    
+}
 
+Pathfinding::Pathfinding(int width, int height, int x_nodes, int y_nodes)
+{
+	this->window.create(sf::VideoMode(width, height), "Pathfinding");
 }
 
 Pathfinding::~Pathfinding()
@@ -10,7 +16,20 @@ Pathfinding::~Pathfinding()
 
 }
 
+void Pathfinding::loop() {
+	while(this->window.isOpen()) {
+        sf::Event event;
+        while (this->window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                this->window.close();
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    
+    Pathfinding pathfinding;
+    pathfinding.loop();
 }
